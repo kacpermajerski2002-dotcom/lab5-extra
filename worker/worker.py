ef9b0a415fed89dd79185bec889de8ca1558d161
kacpermajerski2002-dotcom <kacper.majerski2002@gmail.com>
2026-01-17 08:00:00 +0000

    Add full project files for Lab5

---
+++ worker/worker.py
@@ -0,0 +1,11 @@
+import redis
+import os
+import sys  # dodajemy sys
+
+redis_host = os.getenv("REDIS_HOST", "redis")
+r = redis.Redis(host=redis_host, port=6379, decode_responses=True)
+
+while True:
+    task = r.brpop("tasks")
+    print("Wykonano zadanie:", task, flush=True)  # <-- flush=True
+    sys.stdout.flush()  # wymusza natychmiastowy zapis logu
